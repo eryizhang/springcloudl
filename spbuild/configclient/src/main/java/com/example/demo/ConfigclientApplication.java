626b21e5585e4af73999c3f8dc8b7cb21fb22f5d
ZhangYaHui <ZhangYaHui@SSWD001.SSW.local>
2019-06-10 13:49:30 +0800

    配置项自动刷新

--- spbuild/configclient/src/main/java/com/example/demo/ConfigclientApplication.java
+++ spbuild/configclient/src/main/java/com/example/demo/ConfigclientApplication.java
@@ -3,17 +3,20 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@RefreshScope
 public class ConfigclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
-	@Value("${test}") // git配置文件里的key
+	//@Value("${test-dev.adfasdfa}") // git配置文件里的key
+	@Value("${adfasdfa}")
 			String test;
 
 	@RequestMapping(value = "/hi")
